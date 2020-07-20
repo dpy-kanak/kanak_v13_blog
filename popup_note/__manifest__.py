93895a76f4eac3cadaf48c80087f05c88bc8bf0f
dpy-kanak <dpy@kanakinfosystems.com>
2020-07-20 14:43:53 +0530

    [ADD].

---
+++ popup_note/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Popup Note And Store Value',
+    'version': '13.0',
+    'summary': '',
+    'sequence': 30,
+    'description': """ """,
+    'website': 'www.kanakinfosystems.com',
+    'author': 'Kanak Infosystems LLP',
+    'images': [],
+    'depends': ['base', 'pos_restaurant', 'point_of_sale'],
+    'data': [
+            'views/pos.xml',
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
